69ab854bf88c8192e36ee734d81980cd98a893bf
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-09-20 14:01:40 +1200

    publish types correctly

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import { CommitGraph } from "./dist/types"; // Update the path accordingly
-declare module "commit-graph" {
+declare module "@dreamcatcher-tech/commit-graph" {
   export { CommitGraph };
 }
